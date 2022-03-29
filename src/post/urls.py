77a4122e09618544a22183590ae06295545afea7
jmcb7344 <castrojuan1990@gmail.com>
2022-03-28 22:38:03 -0300

    Vista CreateView

--- src/post/urls.py
+++ src/post/urls.py
@@ -6,5 +6,6 @@ app_name = 'post'
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('nuevo', views.NuevoUser.as_view(), name='nuevousuario'),
-    path('postdetail/<int:pk>', views.PostDetail.as_view(), name='postdetail')
+    path('postdetail/<int:pk>', views.PostDetail.as_view(), name='postdetail'),
+    path('create', views.CreatePost.as_view(), name='createpost')
 ]
